cbc4e0eb8029093f64e106e446ea3feae7719172
Banele Ngwane <banelen@innovise.co.za>
2020-07-17 20:09:27 +0200

    Updates

---
+++ precache-manifest.82ac2bda1b1433b6c40975bc4ba3d91e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e95e65836d9af318a8edb945e0c122",
+    "url": "/index.html"
+  },
+  {
+    "revision": "200f362969a721653eba",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ab3684bbc6d0fc8d1118",
+    "url": "/static/css/main.067c09f2.chunk.css"
+  },
+  {
+    "revision": "200f362969a721653eba",
+    "url": "/static/js/2.590ff444.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.590ff444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab3684bbc6d0fc8d1118",
+    "url": "/static/js/main.3cd94fdd.chunk.js"
+  },
+  {
+    "revision": "b83cbc8f1b0516bb5a45",
+    "url": "/static/js/runtime-main.39d042f1.js"
+  }
+]);
